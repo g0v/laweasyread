c3f527f2a8f7c97bb1765a9c86d8dc7b84c2d5af
kcliu <kcliu@aol.tw>
2013-03-24 02:18:18 +0800

    modified homepage msg

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+var msg = "API endpoint at <p>/law/(law_abbr_name)</p>";
 app.get('/', function(req, res){
-	  res.send('Hello World');
+	  res.send(msg);
 });
 var port = process.env.PORT || 3000;
 app.listen(port);
