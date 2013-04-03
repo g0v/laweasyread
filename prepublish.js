0b265a0494723a761608814a9324cdcc33a2195e
ChangZhuo Chen <czchen@gmail.com>
2013-04-04 02:54:12 +0800

    Compile LiveScript in lib

--- prepublish.js
+++ prepublish.js
@@ -6,5 +6,6 @@ var jade = ['node_modules', '.bin', 'jade'].join(path.sep);
 
 shell.exec(lsc + " -cj package.ls");
 shell.exec(lsc + " -c server");
+shell.exec(lsc + " -c lib");
 
 shell.exec(lsc + " -c public");
