fc683b5b7327d5469f693e96fa55fed042c75761
ChangZhuo Chen <czchen@gmail.com>
2013-04-11 22:25:48 +0800

    Run `npm i' or `mocha' when modify related *.ls

--- gruntfile.js
+++ gruntfile.js
@@ -1,29 +1,51 @@
 'use strict';
 
 module.exports = function(grunt) {
-    grunt.registerTask('livescript', 'compile livescript', function () {
+    var path = require('path');
+    var shell = require('shelljs');
+
+    var bin = ['node_modules', '.bin'].join(path.sep);
+
+    var lsc = [bin, 'lsc'].join(path.sep);
+    var mocha = [bin, 'mocha'].join(path.sep);
+    var npm = 'npm';
+
+    grunt.registerTask('livescript_src', 'update LiveScript source', function () {
         var done = this.async();
-
-        var shell = require('shelljs');
-        var path = require('path');
-        var lsc = ['node_modules', '.bin', 'lsc'].join(path.sep);
-
         // FIXME: Compile changed file only
-        shell.exec(lsc + " -cj package.ls");
-        shell.exec(lsc + " -c lib");
-        shell.exec(lsc + " -c test");
+        shell.exec(lsc + ' -c lib');
+        shell.exec(lsc + ' -c test');
+        grunt.task.run('mocha');
+        done();
+    });
 
+    grunt.registerTask('package_ls', 'update package.ls', function () {
+        var done = this.async();
+        shell.exec(lsc + ' -cj package.ls');
+        shell.exec(npm + ' install');
+        done();
+    });
+
+    grunt.registerTask('mocha', 'run mocha', function () {
+        var done = this.async();
+        shell.exec(mocha);
         done();
     });
 
     grunt.initConfig({
         watch: {
             livescript_src: {
-                files: ['package.ls', 'lib/**/*.ls', 'test/**/*.ls'],
-                tasks: ['livescript']
+                files: ['lib/**/*.ls', 'test/**/*.ls'],
+                tasks: ['livescript_src']
             },
+            package_ls: {
+                files: ['package.ls'],
+                tasks: ['package_ls']
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch']);
 };
